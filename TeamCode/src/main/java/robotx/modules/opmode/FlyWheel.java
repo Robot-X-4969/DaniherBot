8cc72897dc7d180b2c904802cff68b7b64957c92
John <113923030+Indubitably8@users.noreply.github.com>
2026-01-03 15:28:35 -0500

    FlyWheel Test Code

--- TeamCode/src/main/java/robotx/modules/opmode/FlyWheel.java
+++ TeamCode/src/main/java/robotx/modules/opmode/FlyWheel.java
@@ -1,10 +1,8 @@
 package robotx.modules.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import robotx.stx_libraries.XModule;
-import robotx.stx_libraries.components.XGamepad;
 import robotx.stx_libraries.components.XMotor;
 
 public class FlyWheel extends XModule {
@@ -12,12 +10,7 @@ public class FlyWheel extends XModule {
     XMotor motor1;
     XMotor motor2;
 
-    Servo servo;
-
-    boolean isOpen = false;
-
-    int index = 0;
-    static final double[] speeds = {0.0, 1.0, 2.0, 3.0};
+    boolean run = false;
 
     public FlyWheel(OpMode op) {
         super(op);
@@ -26,53 +19,36 @@ public class FlyWheel extends XModule {
     @Override
     public void init() {
         motor1 = new XMotor(opMode, "flyWheel1", 0);
+        motor1.init();
+        motor1.setBrakes(false);
+
         motor2 = new XMotor(opMode, "flyWheel2", 0);
-        servo = opMode.hardwareMap.get(Servo.class, "gate");
-
-        loopMotors.add(motor1);
-        loopMotors.add(motor2);
-
-        motor1.setPower(0.5);
-        motor2.setPower(-0.5);
-
-        servo.setPosition(0.0);
+        motor2.init();
+        motor2.setBrakes(false);
 
         super.init();
     }
 
     @Override
-    public void loop(){
+    public void loop() {
         super.loop();
-
-        motor1.setRPM(speeds[index]);
-        motor2.setRPM(-speeds[index]);
+        if(run){
+            motor1.setPower(-1);
+            motor2.setPower(1);
+        }
+        else {
+            motor1.setPower(0);
+            motor2.setPower(0);
+        }
 
     }
 
     @Override
     public void control_loop(){
         super.control_loop();
-        if(xGamepad1.dpad_left.wasPressed()){
-            index = Math.max(index-1, 0);
-        }
-        else if(xGamepad1.dpad_right.wasPressed()){
-            index = Math.max(index+1, 3);
-        }
-
-        if(xGamepad1.b.wasPressed()){
-            isOpen = !isOpen;
-            setServo();
+        if(xGamepad1.a.wasPressed()){
+            run = !run;
         }
     }
-    
-    public void setServo(){
-        if(isOpen){
-            servo.setPosition(1.0);
-        }
-        else{
-            servo.setPosition(0.0);
-        }
-    }
-
 
 }
--- TeamCode/src/main/java/robotx/opmodes/OpMode2026.java
+++ TeamCode/src/main/java/robotx/opmodes/OpMode2026.java
@@ -3,14 +3,15 @@ package robotx.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import robotx.modules.opmode.DriveSystem;
+import robotx.modules.opmode.FlyWheel;
 import robotx.stx_libraries.XOpMode;
 
 @TeleOp(name = "OpMode 25-26", group = "CurrentOp")
 public class OpMode2026 extends XOpMode {
-    DriveSystem driveSystem;
+    FlyWheel flyWheel;
 
     public void initModules() {
-        driveSystem = new DriveSystem(this);
-        activeModules.add(driveSystem);
+        flyWheel = new FlyWheel(this);
+        activeModules.add(flyWheel);
     }
 }
